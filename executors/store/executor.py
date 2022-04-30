152598d20b257ef6c4225eb663d97f40712e7f8d
Han Xiao <han.xiao@jina.ai>
2022-04-30 22:50:19 +0200

    chore: first commit

--- executors/store/executor.py
+++ executors/store/executor.py
@@ -1,5 +1,3 @@
-import os.path
-
 from jina import Executor, requests, DocumentArray
 
 
@@ -7,16 +5,11 @@ class MyStore(Executor):
 
     def __init__(self, store_path: str, **kwargs):
         super().__init__(**kwargs)
-        self.store_path = store_path
-        if os.path.exists(self.store_path):
-            self.storage = DocumentArray.load_binary(self.store_path)
-        else:
-            self.storage = DocumentArray()
+        self.storage = DocumentArray(storage='sqlite', config={'connection': store_path, 'table_name': 'dalle'})
 
     @requests
     def store(self, docs: DocumentArray, **kwargs):
         self.storage.extend(docs)
-        self.storage.save_binary(self.store_path)
         print(f'total: {len(self.storage)}')
 
     @requests(on='/showall')
