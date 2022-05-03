f7760ffbe3ee2a04869f25d535043f2c89acd2a5
Han Xiao <han.xiao@jina.ai>
2022-05-03 13:29:58 +0200

    fix: multi user collid on glid3 and swinir

--- executors/glid3/executor.py
+++ executors/glid3/executor.py
@@ -40,7 +40,7 @@ class GLID3Diffusion(Executor):
             kw_str = ' '.join(f'--{k} {str(v)}' for k, v in kw.items())
             print('diffusion...')
             print(subprocess.getoutput(f'python sample.py {kw_str}'))
-            for f in glob.glob(f'{self.glid3_path}/output/*.png'):
+            for f in glob.glob(f'{self.glid3_path}/output/{d.id}*.png'):
                 kw['ctime'] = os.path.getctime(f)
                 _d = Document(uri=f, tags=kw).convert_uri_to_datauri()
                 d.matches.append(_d)
