2a11c3a029001f0378f4b1031f6709a7c06f0d66
Han Xiao <han.xiao@jina.ai>
2022-05-06 00:49:41 +0200

    chore: update client

--- executors/glid3/executor.py
+++ executors/glid3/executor.py
@@ -10,10 +10,12 @@ from jina import Executor, DocumentArray, Document, requests
 class GLID3Diffusion(Executor):
     def __init__(self, glid3_path: str, steps: int, **kwargs):
         super().__init__(**kwargs)
+        from dalle_flow_glid3.cli_parser import parser
+        args = parser.parse_args(['--steps', str(steps)])
         self.diffusion_steps = steps
         os.environ['GLID_MODEL_PATH'] = glid3_path
         from dalle_flow_glid3.sample import static_args
-        print(static_args)
+        print(args, static_args)
 
     def run_glid3(self, d: Document, text: str, skip_rate: float, num_images: int):
         with tempfile.NamedTemporaryFile(
