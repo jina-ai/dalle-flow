78b837c95ace95c09967c755297850a7df5aea81
Han Xiao <han.xiao@jina.ai>
2022-05-05 22:55:09 +0200

    chore: update client

--- executors/glid3/executor.py
+++ executors/glid3/executor.py
@@ -1,9 +1,7 @@
 import glob
 import os
 import shutil
-import subprocess
 import tempfile
-from pathlib import Path
 from typing import Dict
 
 from jina import Executor, DocumentArray, Document, requests
@@ -12,47 +10,38 @@ from jina import Executor, DocumentArray, Document, requests
 class GLID3Diffusion(Executor):
     def __init__(self, glid3_path: str, steps: int, **kwargs):
         super().__init__(**kwargs)
-        self.glid3_path = glid3_path
         self.diffusion_steps = steps
+        os.environ['GLID_MODEL_PATH'] = glid3_path
+        from dalle_flow_glid3.sample import static_args
+        self.default_args = static_args
+        self.default_args.steps = self.diffusion_steps
 
-    def run_glid3(self, d: Document, text: str, skip_rate: float):
-        os.chdir(self.glid3_path)
+        self.default_args.num_batches = 1
+
+    def run_glid3(self, d: Document, text: str, skip_rate: float, num_images: int):
         with tempfile.NamedTemporaryFile(
-            suffix='.png',
+                suffix='.png',
         ) as f_in:
             print(f'diffusion [{text}] ...')
             d.save_uri_to_file(f_in.name)
-            shutil.rmtree(f'{self.glid3_path}/output_npy', ignore_errors=True)
+            self.default_args.init_image = f_in.name
+            self.default_args.skip_timesteps = int(self.diffusion_steps * skip_rate)
+            self.default_args.text = text
+            self.default_args.batch_size = num_images
+            self.default_args.output_path = d.id
 
-            Path(f'{self.glid3_path}/output').mkdir(parents=True, exist_ok=True)
-            Path(f'{self.glid3_path}/output_npy').mkdir(parents=True, exist_ok=True)
-
-            kw = {
-                'init_image': f_in.name,
-                'skip_timesteps': int(self.diffusion_steps * skip_rate),
-                'steps': self.diffusion_steps,
-                'model_path': 'finetune.pt',
-                'batch_size': 4,
-                'num_batches': 4,
-                'text': f'"{text}"',
-                'prefix': d.id,
-            }
-            kw_str = ' '.join(f'--{k} {str(v)}' for k, v in kw.items())
-            print(subprocess.getoutput(f'python sample.py {kw_str}'))
-            for f in glob.glob(f'{self.glid3_path}/output/{d.id}*.png'):
-                kw['ctime'] = os.path.getctime(f)
-                _d = Document(uri=f, tags=kw, text=d.text).convert_uri_to_datauri()
+            for f in glob.glob(f'{self.default_args.output_path}/*.png'):
+                _d = Document(uri=f, text=d.text).convert_uri_to_datauri()
                 d.matches.append(_d)
 
             # remove all outputs
-            for f in glob.glob(f'{self.glid3_path}/output/{d.id}*.png'):
-                if os.path.isfile(f):
-                    os.remove(f)
+            shutil.rmtree(self.default_args.output_path, ignore_errors=True)
 
             print('done!')
 
     @requests(on='/diffuse')
     async def diffusion(self, docs: DocumentArray, parameters: Dict, **kwargs):
         skip_rate = float(parameters.get('skip_rate', 0.5))
+        num_images = max(1, min(8, int(parameters.get('num_images', 1))))
         for d in docs:
-            self.run_glid3(d, d.text, skip_rate=skip_rate)
+            self.run_glid3(d, d.text, skip_rate=skip_rate, num_images=num_images)
