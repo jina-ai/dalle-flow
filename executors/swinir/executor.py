631ce0d935a7791099082a285fac4ebff9b3b04c
Han Xiao <han.xiao@jina.ai>
2022-05-07 00:58:33 +0200

    fix: notebook hints

--- executors/swinir/executor.py
+++ executors/swinir/executor.py
@@ -34,7 +34,7 @@ class SwinIRUpscaler(Executor):
         }
         kw_str = ' '.join(f'--{k} {str(v)}' for k, v in kw.items())
 
-        subprocess.getoutput(f'python main_test_swinir.py --large_model {kw_str}')
+        print(subprocess.getoutput(f'python main_test_swinir.py --large_model {kw_str}'))
         d.uri = os.path.join(self.output_path, f'{d.id}_SwinIR.png')
         d.convert_uri_to_datauri()
         d.tags['upscaled'] = True
