a06519d0c4cff3c35c22e9dbe860b9aa86ffa69b
Han Xiao <han.xiao@jina.ai>
2022-05-07 19:36:27 +0200

    fix: db lock

--- executors/store/executor.py
+++ executors/store/executor.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from jina import Executor, requests, DocumentArray
 
 
@@ -5,18 +7,12 @@ class MyStore(Executor):
 
     def __init__(self, store_path: str, **kwargs):
         super().__init__(**kwargs)
-        self.storage = DocumentArray(storage='sqlite', config={'connection': store_path, 'table_name': 'dallemega'})
+        now = datetime.now()
+        table_name = now.strftime('%Y%d%m%H%M%S')
+        self.storage = DocumentArray(storage='sqlite', config={'connection': store_path, 'table_name': table_name})
 
     @requests(on='/')
     def store(self, docs: DocumentArray, **kwargs):
         docs[...].blobs = None  # remove all blobs from anywhere to save space
         self.storage.extend(docs)
         print(f'total: {len(self.storage)}')
-
-    @requests(on='/showall')
-    def showall(self, docs: DocumentArray, **kwargs):
-        return self.storage
-
-    @requests(on='/upload')
-    def upload(self, docs: DocumentArray, **kwargs):
-        self.storage.push('dalle')
