457318d87ee650c9391bc9419873866aaf635c15
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-05-26 03:09:09 +0200

    chore: add debugging log after clip

--- executors/rerank/executor.py
+++ executors/rerank/executor.py
@@ -12,11 +12,15 @@ class ReRank(Executor):
     async def rerank(self, docs: DocumentArray, **kwargs):
         print('re-rank, requests:', {d.tags['request'] for d in docs})
         for d in docs:
-            print('re-rank, request:', d.tags['request'], 'received matches datauris:', '\n'.join([
+            print('re-rank, received request:', d.tags['request'], 'received matches datauris:', '\n'.join([
                 m.uri for m in d.matches
             ]))
 
         print(docs.texts)
         docs = await self._client.arank(docs)
         print(docs.texts)
+        for d in docs:
+            print('re-rank, processed request (after clip):', d.tags['request'], 'received matches datauris:', '\n'.join([
+                m.uri for m in d.matches
+            ]))
         return docs
