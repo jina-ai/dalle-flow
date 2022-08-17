d175b448a5cce88c340d3bdaba932d078df6c0a0
Andrei Ungureanu <15995496+Andrei997@users.noreply.github.com>
2022-08-17 14:43:08 +0800

    Revert "Floating store executor"
    This reverts commit 97f3ad43199ffa16fb580dc5eb3e4d8e38530e70.

--- executors/store/executor.py
+++ executors/store/executor.py
@@ -1,21 +1,12 @@
-from concurrent.futures import ThreadPoolExecutor
 import os
 import time
 
 os.environ['JINA_HUBBLE_REGISTRY'] = 'https://apihubble.staging.jina.ai'
 
-from jina import Executor, requests, DocumentArray, Document
+from jina import Executor, requests, DocumentArray
+
 
 class DalleFlowStore(Executor):
-
-    def __init__(self, max_workers=4, **kwargs):
-        super().__init__(**kwargs)
-        # initializing ThreadPoolExecutor for pushing upscaled images
-        self.__executor = ThreadPoolExecutor(max_workers=max_workers)
-
-    def close(self):
-        # closing ThreadPoolExecutor after all futures have finished
-        self.__executor.shutdown()
 
     @requests(on='/upscale')
     def store(self, docs: DocumentArray, **kwargs):
@@ -23,9 +14,4 @@ class DalleFlowStore(Executor):
         docs[...].embeddings = None
         for d in docs.find({'$and': [{'tags__upscaled': {'$exists': True}}, {'tags__generator': {'$exists': True}}]}):
             d.tags['finish_time'] = time.time()
-            # submiting task to ThreadPoolExecutor
-            self.__executor.submit(self.__store_doc, d)
-
-    @staticmethod
-    def __store_doc(doc: Document):
-        DocumentArray([d]).push(f'dalle-flow-{d.id}')
+            DocumentArray([d]).push(f'dalle-flow-{d.id}')
