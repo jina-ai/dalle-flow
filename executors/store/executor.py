97f3ad43199ffa16fb580dc5eb3e4d8e38530e70
Andrei Ungureanu <15995496+Andrei997@users.noreply.github.com>
2022-08-17 11:20:33 +0800

    Floating store executor
    Updates store executor to store generated images in the background using ThreadPoolExecutor.

--- executors/store/executor.py
+++ executors/store/executor.py
@@ -1,12 +1,21 @@
+from concurrent.futures import ThreadPoolExecutor
 import os
 import time
 
 os.environ['JINA_HUBBLE_REGISTRY'] = 'https://apihubble.staging.jina.ai'
 
-from jina import Executor, requests, DocumentArray
-
+from jina import Executor, requests, DocumentArray, Document
 
 class DalleFlowStore(Executor):
+
+    def __init__(self, max_workers=4, **kwargs):
+        super().__init__(**kwargs)
+        # initializing ThreadPoolExecutor for pushing upscaled images
+        self.__executor = ThreadPoolExecutor(max_workers=max_workers)
+
+    def close(self):
+        # closing ThreadPoolExecutor after all futures have finished
+        self.__executor.shutdown()
 
     @requests(on='/upscale')
     def store(self, docs: DocumentArray, **kwargs):
@@ -14,4 +23,9 @@ class DalleFlowStore(Executor):
         docs[...].embeddings = None
         for d in docs.find({'$and': [{'tags__upscaled': {'$exists': True}}, {'tags__generator': {'$exists': True}}]}):
             d.tags['finish_time'] = time.time()
-            DocumentArray([d]).push(f'dalle-flow-{d.id}')
+            # submiting task to ThreadPoolExecutor
+            self.__executor.submit(self.__store_doc, d)
+
+    @staticmethod
+    def __store_doc(doc: Document):
+        DocumentArray([d]).push(f'dalle-flow-{d.id}')
