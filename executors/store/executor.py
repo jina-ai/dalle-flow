7f21727b07eca2f5b45863de2820f8724d732230
Delgermurun Purevkhuu <delgermurun@users.noreply.github.com>
2022-08-22 11:12:03 +0200

    fix: add random string to docarray name

--- executors/store/executor.py
+++ executors/store/executor.py
@@ -1,5 +1,6 @@
-import os
 import time
+import string
+import random
 
 from jina import Executor, requests, DocumentArray
 
@@ -8,8 +9,9 @@ class DalleFlowStore(Executor):
 
     @requests(on='/upscale')
     def store(self, docs: DocumentArray, **kwargs):
+        random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
         docs[...].blobs = None  # remove all blobs from anywhere to save space
         docs[...].embeddings = None
         for d in docs.find({'$and': [{'tags__upscaled': {'$exists': True}}, {'tags__generator': {'$exists': True}}]}):
             d.tags['finish_time'] = time.time()
-            DocumentArray([d]).push(f'dalle-flow-{d.id}')
+            DocumentArray([d]).push(f'dalle-flow-{d.id}-{random_str}')
